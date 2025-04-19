0b74250958f6434711653c1c64e1745a4b58bbe8
Samir Rodas <serodas9710@gmail.com>
2025-04-19 13:08:40 -0500

    Add ScopeAmbassadorMiddleware for enhanced role-based access control

--- microservices/ambassador/routes/api.php
+++ microservices/ambassador/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\ScopeAmbassadorMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('ambassador')->group(function () {
@@ -8,8 +9,10 @@ Route::prefix('ambassador')->group(function () {
         ->name('register');
     Route::post('login', [AuthController::class, 'login'])
         ->name('login');
-    Route::get('user', [AuthController::class, 'user']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::put('users/info', [AuthController::class, 'updateInfo']);
-    Route::put('users/password', [AuthController::class, 'updatePassword']);
+    Route::middleware(ScopeAmbassadorMiddleware::class)->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::put('users/info', [AuthController::class, 'updateInfo']);
+        Route::put('users/password', [AuthController::class, 'updatePassword']);
+    });
 });
