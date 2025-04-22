cf3f3407caab0c3a29edb1bf8d06822d82f7f10d
Samir Rodas <serodas9710@gmail.com>
2025-04-21 22:06:50 -0500

    Add controllers in admin microservices

--- microservices/admin/routes/api.php
+++ microservices/admin/routes/api.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\AmbassadorController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\ScopeAdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('checkout')->group(function () {
-    Route::get('links/{code}', [LinkController::class, 'show']);
-    Route::post('orders', [OrderController::class, 'store']);
-    Route::post('orders/confirm', [OrderController::class, 'confirm']);
+Route::prefix('admin')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware(ScopeAdminMiddleware::class)->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::put('users/info', [AuthController::class, 'updateInfo']);
+        Route::put('users/password', [AuthController::class, 'updatePassword']);
+
+        Route::get('ambassadors', [AmbassadorController::class, 'index']);
+        Route::get('users/{id}/links', [LinkController::class, 'index']);
+        Route::get('orders', [OrderController::class, 'index']);
+
+        Route::apiResource('products', ProductController::class);
+    });
 });
